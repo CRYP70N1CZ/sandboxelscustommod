9408a7da7607f49fde98d1aaef878c3f67195c86
zpaqs <CRYP70N1CZ@proton.me>
2025-06-15 14:35:53 -0500

    Create Neutroniumall.js

---
+++ Neutroniumall.js
@@ -0,0 +1,17 @@
+elements.neutronium = {
+    color: "#2b2b2b",
+    behavior: behaviors.WALL,
+    category: "solids",
+    state: "solid",
+    density: 99999,
+    hardness: 1,
+    insulate: true,
+    tempHigh: 1e10, // basically cannot melt
+    breakInto: null,
+    burn: 0,
+    burnTime: 0,
+    noMix: true,
+    hardness: 1,
+    blastResistance: 1e10, // crucial
+    excludeRandom: true, // optional
+};
